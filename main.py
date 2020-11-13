41fb25bb7e1c7aa5e8dfad09ab8e257fbeca9487
Anastasiia Romanenko <druzhkoshowewed@gmail.com>
2020-11-13 22:19:19 +0300

    second commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import sys
+from random import randint
+
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.do_paint = False
+        self.btn.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 201, 14))
+        r = randint(2, 100)
+        qp.drawEllipse(randint(100, 600), randint(100, 600), r, r)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
