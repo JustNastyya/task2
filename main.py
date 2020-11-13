ef59e05753d67965724e665046966be5742323dc
Anastasiia Romanenko <druzhkoshowewed@gmail.com>
2020-11-13 22:35:37 +0300

    third commit

--- main.py
+++ main.py
@@ -2,14 +2,25 @@ import sys
 from random import randint
 
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtWidgets import QLCDNumber, QLabel, QLineEdit
 
 
-class MyWidget(QMainWindow):
+class Example(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 100, 800, 800)
+        self.btn = QPushButton('Кнопка', self)
+        self.btn.resize(self.btn.sizeHint())
+        self.btn.move(380, 20)
+
+
+class NewWidget(Example):
+    def __init__(self):
+        super().__init__()
         self.do_paint = False
         self.btn.clicked.connect(self.paint)
 
@@ -25,13 +36,13 @@ class MyWidget(QMainWindow):
         self.repaint()
 
     def draw_flag(self, qp):
-        qp.setBrush(QColor(255, 201, 14))
-        r = randint(2, 100)
+        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
+        r = randint(2, 300)
         qp.drawEllipse(randint(100, 600), randint(100, 600), r, r)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = MyWidget()
+    ex = NewWidget()
     ex.show()
     sys.exit(app.exec_())
